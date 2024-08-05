0bf4e196494122ced58ddf455d85dc1c0edc17dd
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2024-08-05 00:31:57 -0400

    message

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,7 @@ import React from 'react';
 const Header = () => {
 
     return (
-        <header className="header__wrapper">
-            <div className="header__wrapper--mid">
-
-                <h1>FEEME</h1>
-                <sub>Best fashion shoping experiences</sub>
-
-            </div>
+        <header>
             <nav>
                 <ul>
                     <a className="bm-item-list" href="/" activeClassName="active">New Arival</a>
@@ -17,6 +11,25 @@ const Header = () => {
                     <a className="bm-item-list" href="/" activeClassName="active">About</a>
                 </ul>
             </nav>
+
+            <div className="header__wrapper--mid">
+
+                <h1>FEEME</h1>
+                <sub>Best fashion shoping experiences</sub>
+
+            </div>
+            
+            <div className="header__wrapper--right">
+                
+                <button type="button" className="btn btn__primary navbar__btn signup">
+                    Sign up
+                </button>
+                <button type="button" className="btn btn__primary navbar__btn login">
+                    Login
+                </button>
+                
+            </div>
+
         </header>
     );
 };
